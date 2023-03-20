1c23b246fe8f50583c34ad341f5d60e806a0cfb4
Andrii Sachok <andriy.sachok@gmail.com>
2023-03-20 18:50:31 +0200

    added demo navigation-menu

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Stack, Typography } from '@mui/material';
+
+const Navigation = () => {
+  return (
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      direction="row"
+      gap={6}
+      height="4rem"
+    >
+      <Typography variant="h6">Work</Typography>
+      <Typography variant="h6">Resume</Typography>
+      <Typography variant="h6">Contact</Typography>
+    </Stack>
+  );
+};
+
+export default Navigation;
