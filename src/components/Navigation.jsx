1d1ebaf07d59bed7ba32f3534dce288c039e572d
Andrii Sachok <andriy.sachok@gmail.com>
2023-03-21 17:22:40 +0200

    updated navigation-menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Stack, Typography } from '@mui/material';
+import { IconButton, Stack, Typography } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import './Navigation.scss';
 
 const Navigation = () => {
   return (
-    <Stack
-      justifyContent="center"
-      alignItems="center"
-      direction="row"
-      gap={6}
-      height="4rem"
-      sx={{ position: 'absolute', top: '0' }}
-    >
-      <Link to="/work" className="link">
-        <Typography variant="h6">Work</Typography>
-      </Link>
-      <Link to="/resume" className="link">
-        <Typography variant="h6">Resume</Typography>
-      </Link>
-      <Link to="/contact" className="link">
-        <Typography variant="h6">Contact</Typography>
-      </Link>
-    </Stack>
+    <>
+      <Stack
+        justifyContent="space-between"
+        alignItems="center"
+        direction="row"
+        gap={6}
+        height="4rem"
+        width="100%"
+        sx={{ position: 'fixed', top: '0' }}
+      >
+        <Link to="/home" className="link">
+          <HomeIcon sx={{ fontSize: '28px', margin: '0 2rem' }} />
+        </Link>
+
+        <Stack alignItems="inherit" direction="row" gap="inherit">
+          <Link to="/work" className="link">
+            <Typography variant="h6">Work</Typography>
+          </Link>
+          <Link to="/resume" className="link">
+            <Typography variant="h6">Resume</Typography>
+          </Link>
+          <Link to="/contact" className="link">
+            <Typography variant="h6">Contact</Typography>
+          </Link>
+        </Stack>
+        <IconButton
+          disableRipple
+          onClick={() => {
+            window.location.href = 'https://github.com/saachok';
+          }}
+        >
+          <GitHubIcon
+            sx={{ fontSize: '28px', margin: '0 2rem', color: 'black' }}
+          />
+        </IconButton>
+      </Stack>
+    </>
   );
 };
 
