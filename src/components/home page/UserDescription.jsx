fbc7a1fbfab9315a681c4cb926d2dc7e7cd31784
Andrii Sachok <andriy.sachok@gmail.com>
2023-03-23 12:03:17 +0200

    added animations to home-page

---
+++ src/components/home page/UserDescription.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Stack, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+import { title, firstParagraph, secondParagraph } from './animations';
+
+const UserDescription = () => {
+  const bgStyles = {
+    backgroundColor: 'rgb(132,168,192)',
+    color: 'white',
+    padding: '0 6px 3px 1px',
+  };
+  return (
+    <Stack direction="column" gap={1} width="650px">
+      <motion.div
+        initial={title.initial}
+        animate={title.animate}
+        transition={title.transition}
+      >
+        <Typography variant="h3" fontWeight="bold">
+          Hi, I'm Andrii and I'm <br />a
+          <em style={bgStyles}>Frontend Web Developer</em>.
+        </Typography>
+      </motion.div>
+      <Stack direction="column" gap="inherit" width="560px">
+        <motion.div
+          initial={firstParagraph.initial}
+          animate={firstParagraph.animate}
+          transition={firstParagraph.transition}
+        >
+          <Typography variant="body1">
+            I'm a young web developer who likes to create useful products and
+            share them with others on the Internet. My strengths are teamwork,
+            finding alternative ways to solve problems, focus on achieving the
+            best final result.
+          </Typography>
+        </motion.div>
+        <motion.div
+          initial={secondParagraph.initial}
+          animate={secondParagraph.animate}
+          transition={secondParagraph.transition}
+        >
+          <Typography variant="body1">
+            Every day I'm improving my knowledge and I'll be glad to build more
+            and more interesting projects that let me become a master of web
+            development :)
+          </Typography>
+        </motion.div>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default UserDescription;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,71 @@
 import React from 'react';
 import { Stack } from '@mui/material';
 
-import UserDescription from '../components/UserDescription';
+import UserDescription from '../components/home page/UserDescription';
 import CustomButton from '../components/UI/CustomButton';
 import { Link } from 'react-router-dom';
+
+import { motion } from 'framer-motion';
+import { img, buttons } from '../components/home page/animations';
 
 const Home = () => {
   return (
     <Stack direction="row" justifyContent="center" alignItems="center" gap={4}>
-      <img
-        src="./photo-2.jpg"
-        width="350px"
-        alt="user"
-        style={{ borderRadius: '4px' }}
-      />
+      <motion.div
+        initial={img.initial}
+        animate={img.animate}
+        transition={img.transition}
+      >
+        <img
+          src="./photo-2.jpg"
+          width="350px"
+          alt="user"
+          style={{ borderRadius: '4px' }}
+        />
+      </motion.div>
       <Stack direction="column" gap={1}>
         <UserDescription />
-        <Stack direction="row" gap={2} marginTop="1rem">
-          <Link
-            to="/contact"
-            style={{ textDecoration: 'none', color: 'inherit' }}
-          >
-            <CustomButton
-              sx={{
-                backgroundColor: 'rgb(132,168,192)',
-                color: 'white',
-                '&:hover': {
-                  backgroundColor: 'rgb(118, 152, 176)',
-                },
-              }}
+        <motion.div
+          initial={buttons.initial}
+          animate={buttons.animate}
+          transition={buttons.transition}
+        >
+          <Stack direction="row" gap={2} marginTop="1rem">
+            <Link
+              to="/contact"
+              style={{ textDecoration: 'none', color: 'inherit' }}
             >
-              Contact me
-            </CustomButton>
-          </Link>
-          <Link to="/work" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <CustomButton
-              sx={{
-                backgroundColor: 'inherit',
-                border: '1px solid darkgreen',
-                color: 'black',
-                '&:hover': {
-                  backgroundColor: 'rgb(193, 207, 220)',
-                },
-              }}
+              <CustomButton
+                sx={{
+                  backgroundColor: 'rgb(132,168,192)',
+                  color: 'white',
+                  '&:hover': {
+                    backgroundColor: 'rgb(118, 152, 176)',
+                  },
+                }}
+              >
+                Contact me
+              </CustomButton>
+            </Link>
+            <Link
+              to="/work"
+              style={{ textDecoration: 'none', color: 'inherit' }}
             >
-              Check my work
-            </CustomButton>
-          </Link>
-        </Stack>
+              <CustomButton
+                sx={{
+                  backgroundColor: 'inherit',
+                  border: '1px solid darkgreen',
+                  color: 'black',
+                  '&:hover': {
+                    backgroundColor: 'rgb(193, 207, 220)',
+                  },
+                }}
+              >
+                Check my work
+              </CustomButton>
+            </Link>
+          </Stack>
+        </motion.div>
       </Stack>
     </Stack>
   );
