8a91a25af8e54840ad54ceed9a5d079be72a5fac
Andrii Sachok <andriy.sachok@gmail.com>
2023-03-30 16:57:04 +0300

    added resume-page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { theme } from './theme';
 
 import Home from './pages/Home';
 import Work from './pages/Work';
+import Resume from './pages/Resume';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" exact element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/work/*" element={<Work />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
     </ThemeProvider>
   );
