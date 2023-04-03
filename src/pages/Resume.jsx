a78912c4fca2b7cf07f170c544db3f671399064f
Andriy <andriy.sachok@gmail.com>
2023-04-03 13:46:29 +0300

    fixed pdf-reader

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-import samplePDF from '../Resume.pdf';
 import { Container } from '@mui/material';
 
 import Navigation from '../components/Navigation';
 
 const Resume = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
+  const PDF_PATH = process.env.PUBLIC_URL + '/Resume.pdf';
   return (
     <Container disableGutters maxWidth="true">
       <Navigation />
-      <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
+      <Document file={PDF_PATH}>
+        <Page pageNumber={1} />
       </Document>
     </Container>
   );
