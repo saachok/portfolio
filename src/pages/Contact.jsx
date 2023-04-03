28da9809c7877f28cfa5413c909a85b8312b8893
Andriy <andriy.sachok@gmail.com>
2023-04-03 14:48:57 +0300

    added animations to contact-page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-import {
-  Stack,
-  Paper,
-  Typography,
-  Button,
-  Container,
-  Link,
-} from '@mui/material';
+import { Paper, Typography, Button, Container } from '@mui/material';
 import { AlternateEmail, Telegram, LinkedIn } from '@mui/icons-material';
 import '../components/contact page/Contact.scss';
+import { motion } from 'framer-motion';
+import {
+  wrapper,
+  title,
+  linkedIn,
+  telegram,
+  email,
+} from '../components/contact page/animations';
 
 const LinkButton = ({ startIcon, href, children }) => {
   return (
     <Button
+      fullWidth
       variant="outlined"
       startIcon={startIcon}
       href={href}
@@ -46,59 +48,73 @@ const Contact = () => {
       }}
     >
       <Navigation />
-      <Paper
-        elevation={4}
-        sx={{
-          backgroundColor: 'rgb(223, 233, 245)',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '1rem',
-          padding: '1rem',
-        }}
-        // direction="column"
-        // gap={2}
+      <motion.div
+        initial={wrapper.initial}
+        animate={wrapper.animate}
+        transition={wrapper.transition}
       >
-        {/* <Link href={LINKEDIN_URL} className="contact-link" variant="h5">
-          <Stack direction="row" alignItems="center">
-            <LinkedIn className="contact-icon" />
-            LindenIn
-          </Stack>
-        </Link> */}
-        <Typography variant="h4" textAlign="center">
-          Contact me!
-        </Typography>
-        <LinkButton
-          startIcon={<LinkedIn className="contact-icon" />}
-          href={LINKEDIN_URL}
+        <Paper
+          elevation={4}
+          sx={{
+            backgroundColor: 'rgb(223, 233, 245)',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '1rem',
+            padding: '1rem',
+          }}
         >
-          LinkedIn
-        </LinkButton>
-        <LinkButton
-          startIcon={<Telegram className="contact-icon" />}
-          href={TELEGRAM_URL}
-        >
-          Telegram
-        </LinkButton>
-        <LinkButton
-          startIcon={<AlternateEmail className="contact-icon" />}
-          href={GMAIL_URL}
-        >
-          Email
-        </LinkButton>
-        {/* 
-        <Link href={TELEGRAM_URL} className="contact-link" variant="h5">
-          <Stack direction="row" alignItems="center">
-            <Telegram className="contact-icon" />
-            Telegram
-          </Stack>
-        </Link>
-        <Link href={GMAIL_URL} className="contact-link" variant="h5">
-          <Stack direction="row" alignItems="center">
-            <AlternateEmail className="contact-icon" />
-            Gmail
-          </Stack>
-        </Link> */}
-      </Paper>
+          <motion.div
+            initial={title.initial}
+            animate={title.animate}
+            transition={title.transition}
+          >
+            <Typography variant="h4" textAlign="center">
+              Contact me!
+            </Typography>
+          </motion.div>
+          <motion.div
+            initial={linkedIn.initial}
+            animate={linkedIn.animate}
+            transition={linkedIn.transition}
+            className="button-animation"
+          >
+            <LinkButton
+              startIcon={<LinkedIn className="contact-icon" />}
+              href={LINKEDIN_URL}
+            >
+              LinkedIn
+            </LinkButton>
+          </motion.div>
+          <motion.div
+            initial={telegram.initial}
+            animate={telegram.animate}
+            transition={telegram.transition}
+            className="button-animation"
+          >
+            <LinkButton
+              startIcon={<Telegram className="contact-icon" />}
+              href={TELEGRAM_URL}
+            >
+              Telegram
+            </LinkButton>
+          </motion.div>
+          <motion.div
+            initial={email.initial}
+            animate={email.animate}
+            transition={email.transition}
+            className="button-animation"
+          >
+            <LinkButton
+              startIcon={<AlternateEmail className="contact-icon" />}
+              href={GMAIL_URL}
+            >
+              Email
+            </LinkButton>
+          </motion.div>
+        </Paper>
+      </motion.div>
     </Container>
   );
 };
