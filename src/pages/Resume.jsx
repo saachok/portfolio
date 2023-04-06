a9a0434d718732ced767c0f97bb316f93ba5dbe9
Andriy <andriy.sachok@gmail.com>
2023-04-06 16:07:11 +0300

    added download-pdf button

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-import { Container } from '@mui/material';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import { Container, Button } from '@mui/material';
+import { saveAs } from 'file-saver';
+import ResumePDF from '../Resume.pdf';
 
 import Navigation from '../components/Navigation';
 
 const Resume = () => {
   const PDF_PATH = process.env.PUBLIC_URL + '/Resume.pdf';
+
+  const handleDownload = async () => {
+    try {
+      saveAs(ResumePDF, 'Andrii Sachok');
+    } catch (e) {
+      console.error('Error downloading PDF file', e);
+    }
+  };
+
   return (
     <Container disableGutters maxWidth="true">
       <Navigation />
-      <Document file={PDF_PATH}>
-        <Page pageNumber={1} />
+      <Document file={PDF_PATH} disableAnnotationLayer={true}>
+        <Page height={1200} pageNumber={1} />
       </Document>
+      <Button
+        onClick={handleDownload}
+        sx={{
+          margin: '2rem',
+          backgroundColor: 'inherit',
+          border: '1px solid black',
+          color: 'black',
+          '&:hover': {
+            backgroundColor: 'rgb(193, 207, 220)',
+          },
+        }}
+      >
+        Download PDF
+      </Button>
     </Container>
   );
 };
