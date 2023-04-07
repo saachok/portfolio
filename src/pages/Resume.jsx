8e18de1cd971156baaf6927ac29990ca4afa7074
Andriy <andriy.sachok@gmail.com>
2023-04-07 12:48:17 +0300

    added download-pdf button

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+// import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import { Container, Button } from '@mui/material';
 import { saveAs } from 'file-saver';
