20528cd295fd87596fbdbc49afede77951ac2dd9
Andrii Sachok <andriy.sachok@gmail.com>
2023-10-16 19:44:05 +0300

    Added Rick and Morty project
    Squashed commit of the following:
    
    commit be5a4c449b3a39629478b15dcd453c5578dba693
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Mon Oct 16 18:50:17 2023 +0300
    
        Added Rick and Morty project
    
        Squashed commit of the following:
    
        commit 2680ae9f2e09ab8e2ffe35125b684f091e9bad3a
        Author: Andrii Sachok <andriy.sachok@gmail.com>
        Date:   Mon Oct 16 18:39:46 2023 +0300
    
            Added Rick adn Morty project
    
            Squashed commit of the following:
    
            commit fd680cee56cf18ec7279aec72de8d8996b1bf7f7
            Author: Andrii Sachok <andriy.sachok@gmail.com>
            Date:   Mon Oct 16 18:37:53 2023 +0300
    
                Added Rick and Morty project
    
            commit bd159111bda3d57f90e363ae089c71a28b5ec081
            Author: Andrii Sachok <andriy.sachok@gmail.com>
            Date:   Mon Oct 16 16:03:59 2023 +0300
    
                Rebuilt grid architecture
    
    commit 310e6bcb1f5601c5f6542180531088a0820716e3
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Thu Apr 13 20:59:59 2023 +0300
    
        update .pdf file
    
    commit da182a2d113ee45d69819c1353e7400639bd449a
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Thu Mar 30 17:08:15 2023 +0300
    
        update layout for project page
    
    commit 308e689e30180a385255f2be42d900e52ac9d4b8
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Thu Mar 30 17:01:26 2023 +0300
    
        added gh-pages branch
    
    commit ee3b149621579ea89a76bcf8865ade849c65cbdf
    Author: Andrii Sachok <andriy.sachok@gmail.com>
    Date:   Wed Apr 19 21:48:19 2023 +0300
    
        changed layout for project page

--- src/components/work page/ProjectsList.jsx
+++ src/components/work page/ProjectsList.jsx
@@ -3,62 +3,7 @@ import { Grid } from '@mui/material';
 import GridItem from './GridItem';
 import { motion } from 'framer-motion';
 import './styles/Projects.scss';
-
-const repos = [
-  {
-    img: './projects images/spotify-playlist-creator.png',
-    path: 'spotify-playlist-creator',
-    title: 'Spotify Playlist Creator',
-    description:
-      'This project was created for practice with Spotify for Developers API. You need to wait a few minutes after clicking on the image because it’s a free hosting site and application rebuilt after 15 minutes inactive mode.',
-    tools: ['React', 'Express', 'MUI', 'JavaScript'],
-  },
-
-  {
-    img: './projects images/react-food-order.png',
-    path: 'food-order-app',
-    title: 'React Food Order',
-    description:
-      'App was created for practice with a connection to own remote database (e.g. FireBase). As an alternative was connected to Redux instead of sending HTTP requests.',
-    tools: ['React', 'Redux', 'JavaScript', 'HTML', 'CSS'],
-  },
-  {
-    img: './projects images/russian-losses-tracker.png',
-    path: 'russian-losses-tracker',
-    title: 'Russian Losses Tracker',
-    description:
-      'Main goal of this app is to insert a graph that based on dynamic data. GET request was created by date-range-picker that set an offset for custom amount of days. Information fetched by several APIs.',
-
-    tools: ['React', 'Chart.js', 'MUI', 'JavaScript', 'CSS'],
-  },
-  {
-    img: './projects images/breaking-bad-characters.png',
-    path: 'breaking-bad-characters/characters',
-    title: 'Breaking Bad Characters',
-    description:
-      'The goal of this project was to practice with React Router and set different animations to elements. In example I used Material UI components and Motion library to animate these elements.',
-
-    tools: ['React', 'Router', 'Motion', 'JavaScript', 'CSS', 'MUI'],
-  },
-  {
-    img: './projects images/calculator.png',
-    path: 'calculator',
-    title: 'Calculator',
-    description:
-      'This project was created a few years ago without using React. After this time has been successfully imported into React app. Main goal was to practice DOM manipulation via JavaScript.',
-
-    tools: ['JavaScript', 'HTML', 'CSS'],
-  },
-  {
-    img: './projects images/weather-tracker.png',
-    path: 'weather-tracker',
-    title: 'Weather Tracker',
-    description:
-      'Main goal of this project was to practice sending HTTP requests to remote storage via API. This app has a complete layout in HTML and modifies content of dozen elements by using JavaScript DOM manipulation.',
-
-    tools: ['JavaScript', 'HTML', 'CSS'],
-  },
-];
+import { REPOS } from '../../constants';
 
 const ProjectsList = () => {
   let delay = 0;
@@ -72,16 +17,14 @@ const ProjectsList = () => {
           md: '80%',
         }}
       >
-        {repos
-          .map((proj) => {
-            delay += 0.2;
-            return { ...proj, delay };
-          })
-          .map((proj) => (
-            <Grid item lg={4} sm={6} xs={12} key={proj.title}>
-              <GridItem project={proj} />
-            </Grid>
-          ))}
+        {REPOS.map(proj => {
+          delay += 0.2;
+          return { ...proj, delay };
+        }).map(proj => (
+          <Grid item lg={4} sm={6} xs={12} key={proj.title}>
+            <GridItem project={proj} />
+          </Grid>
+        ))}
       </Grid>
     </motion.div>
   );
---
+++ src/constants.js
@@ -0,0 +1,71 @@
+export const REPOS = [
+  {
+    img: './projects images/rick-and-morty-app.png',
+    path: 'rick-and-morty-app',
+    title: 'Rick and Morty GraphQL App',
+    description:
+      'This project was created with Next.js and GraphQL, leveraging the popular Rick and Morty API. This application seamlessly combines the power of the Next.js framework for React applications with the flexibility and efficiency of GraphQL for data retrieval.',
+    tools: ['Next', 'GraphQL', 'React Hooks', 'JavaScript', 'SCSS'],
+    external: true,
+    href: 'https://rick-and-morty-next-graphql-rho.vercel.app/',
+  },
+  {
+    img: './projects images/spotify-playlist-creator.png',
+    path: 'spotify-playlist-creator',
+    title: 'Spotify Playlist Creator',
+    description:
+      'This project was created for practice with Spotify for Developers API. You need to wait a few minutes after clicking on the image because it’s a free hosting site and application rebuilt after 15 minutes inactive mode.',
+    tools: ['React', 'Express', 'MUI', 'JavaScript'],
+    external: true,
+    href: 'https://spotify-playlist-creator.onrender.com/',
+  },
+  {
+    img: './projects images/react-food-order.png',
+    path: 'food-order-app',
+    title: 'React Food Order',
+    description:
+      'App was created for practice with a connection to own remote database (e.g. FireBase). As an alternative was connected to Redux instead of sending HTTP requests.',
+    tools: ['React', 'Redux', 'JavaScript', 'HTML', 'CSS'],
+    external: false,
+  },
+  {
+    img: './projects images/russian-losses-tracker.png',
+    path: 'russian-losses-tracker',
+    title: 'Russian Losses Tracker',
+    description:
+      'Main goal of this app is to insert a graph that based on dynamic data. GET request was created by date-range-picker that set an offset for custom amount of days. Information fetched by several APIs.',
+
+    tools: ['React', 'Chart.js', 'MUI', 'JavaScript', 'CSS'],
+    external: false,
+  },
+  {
+    img: './projects images/breaking-bad-characters.png',
+    path: 'breaking-bad-characters/characters',
+    title: 'Breaking Bad Characters',
+    description:
+      'The goal of this project was to practice with React Router and set different animations to elements. In example I used Material UI components and Motion library to animate these elements.',
+
+    tools: ['React', 'Router', 'Motion', 'JavaScript', 'CSS', 'MUI'],
+    external: false,
+  },
+  {
+    img: './projects images/calculator.png',
+    path: 'calculator',
+    title: 'Calculator',
+    description:
+      'This project was created a few years ago without using React. After this time has been successfully imported into React app. Main goal was to practice DOM manipulation via JavaScript.',
+
+    tools: ['JavaScript', 'HTML', 'CSS'],
+    external: false,
+  },
+  {
+    img: './projects images/weather-tracker.png',
+    path: 'weather-tracker',
+    title: 'Weather Tracker',
+    description:
+      'Main goal of this project was to practice sending HTTP requests to remote storage via API. This app has a complete layout in HTML and modifies content of dozen elements by using JavaScript DOM manipulation.',
+
+    tools: ['JavaScript', 'HTML', 'CSS'],
+    external: false,
+  },
+];
